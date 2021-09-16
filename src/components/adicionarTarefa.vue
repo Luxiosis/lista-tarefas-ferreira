4c7233a4a8b6a21297414adc6062c4b766e38bae
unknown <lucasdudu1001@gmail.com>
2021-09-16 10:56:01 -0300

    Primeiro programa

---
+++ src/components/adicionarTarefa.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="input-group">
+      <!-- <input @keyup.enter="adicionarTarefa" v-model="novaTarefa" type="text" /> -->
+      <!-- <entrada-dados :value="novaTarefa" @input="associarValor" /> -->
+      <entrada-dados @salvar="adicionarTarefa" v-model="novaTarefa" />
+      <span class="input-group-btn">
+        <button @click="adicionarTarefa" class="btn btn-primary">
+          Adicionar novas tarefas
+        </button>
+      </span>
+    </div>
+</template>
+
+<script>
+import entradaDados from '@/components/templates/entradaDados'
+export default {
+    components: { entradaDados },
+    data() {
+        return {
+            novaTarefa: "",
+        };
+    },
+    methods: {
+        adicionarTarefa() {
+            this.$emit("adicionarTarefa", this.novaTarefa);
+            this.novaTarefa = "";
+        },
+        // associarValor(valor) {
+        //    this.novaTarefa = valor;
+        //},
+    },
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/listarTarefas.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul>
+      <li 
+        v-for="(tarefa, index) in tarefas" 
+        :key="index" 
+        :class="{ removed: tarefa.checked }"
+      >
+        <input type="checkbox" v-model="tarefa.checked">
+        <label>{{ tarefa.titulo }}</label>
+      </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: {
+        tarefas: {
+            type: Array,
+            default() {
+                return [{titulo: "Atividade Padrão", checked: false}]
+            },
+        },
+    },
+};
+</script>
+
+<style>
+
+</style>
